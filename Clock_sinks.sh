9c9ab83dfb47b169cf7a4a4e7a8dcc976d26dc1f
Rajat Govil <91873360+Rajat-Govil@users.noreply.github.com>
2023-04-23 22:17:12 +0530

    Update Clock_sinks.sh

--- Clock_sinks.sh
+++ Clock_sinks.sh
@@ -4,16 +4,16 @@ read -p "Enter the file name:" filename
 if [ -e $filename ];
 then
 line_no=`egrep -in -A2 'Skew Group Structure' $filename | tail -1 | awk -F- '{print $1}'`
-total_lines=`wc -l sg_new.rpt | awk '{print $1}'`
+total_lines=`wc -l $filename | awk '{print $1}'`
 
 #iterate over the lines till the next blank line is found
 for ((i=$line_no+1; i<$total_lines; i=i+1))
 do 
-	sed -n "${i}p" sg_new.rpt | grep '^$' 
+	sed -n "${i}p" $filename | grep '^$' 
 	exitstat=$?	
 	if [ $exitstat -eq 0 ];
 	then
-		sed -n "${line_no},${i}p" sg_new.rpt 
+		sed -n "${line_no},${i}p" $filename 
 		break
 	else
 		continue
